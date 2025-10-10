e27e12f556aad1c550241303cce05c11e617a640
Muten-Roshi-Sama <21245@ecam.be>
2025-10-10 11:55:15 +0200

    JSON structure + base for all entities (users, artists, tracks, albums,...)'

---
+++ SoundSync/backend/app/db/collections.py
@@ -0,0 +1,60 @@
+# exercises/db_connect.py
+from pymongo import MongoClient
+import pprint
+import json, os
+import app.core.events as events
+
+MC = events.mongo_client
+REDIS = events.redis_client
+
+
+
+ARTISTS = MC["artists"]
+ALBUMS = MC["albums"]
+TRACKS = MC["tracks"]
+CONCERTS = MC["concerts"]
+GENRES = MC["genres"]
+
+USERS = MC["users"]
+LIKES = MC["likes"]
+COMMENTS = MC["comments"]
+PLAYLISTS = MC["playlists"]
+SUBSCRIPTIONS = MC["subscriptions"]
+
+
+
+
+# def get_db():
+#     return MC["moviesdb"]
+
+# def get_movies_collection():
+#     return get_db()["movies"]
+
+# def showDB():
+#     """Prints all documents in the movies collection."""
+#     movies = get_movies_collection()
+#     all_movies = list(movies.find())
+    
+#     if not all_movies:
+#         print("Database empty (movies collection).")
+#         return
+    
+#     pprint.pprint(all_movies)
+
+
+# def cleanup(print):
+#     db = get_db()
+#     db.drop_collection("movies")
+#     if print : print("[cleanup] Dropped 'movies' collection.")
+#     showDB()
+
+
+
+
+
+
+
+
+
+
+
